b496df1506ea9abf7a0dd26d5b59cf7d4c2da20f
zhengzilong02@meituan.com <zhengzilong02@meituan.com>
2022-09-15 15:21:49 +0800

    fix

--- config/index.js
+++ config/index.js
@@ -27,7 +27,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oIjGO5xU66sA_NTRNAlK99NgqfzY",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "JjrgxWjgwgBb9MdldA9wzNZgs4CMTHxigPGZamBnZ44",
+      useTemplateId: "PXYgihtwNW4d3qCLmQt2zfsjCj7JdUHA-FcfyqHwM2Q",
       // 所在省份
       province: "北京",
       // 所在城市
